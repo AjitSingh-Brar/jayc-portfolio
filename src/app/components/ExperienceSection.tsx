1e5d66e1b837519de242bcd2b5b2fe948d7d7e3f
NS918326 <Navrajgaltee@outlook.com>
2024-06-01 17:17:10 -0400

    UPDT: code refactored

--- src/app/components/ExperienceSection.tsx
+++ src/app/components/ExperienceSection.tsx
@@ -70,7 +70,7 @@ const ExperienceSection = () => {
         </ol>
         <div className="mt-12">
           <Link
-            href="https://acrobat.adobe.com/id/urn:aaid:sc:va6c2:59346cb8-811f-4221-a279-873f4a3b8386"
+            href="https://acrobat.adobe.com/id/urn:aaid:sc:va6c2:e5a97365-b439-49d5-83a9-d0dc1c31aa87"
             className="inline-flex items-baseline font-medium leading-tight text-slate-200 hover:text-sky-300 focus-visible:text-sky-300 group/link text-base duration-200 ease-in-out"
             target="_blank"
             rel="noreferrer noopener"
